ba29fd7fcedb14d7e2dc515a0c655c73a2da2e4f
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 11:08:18 +0700

    Update cart items

--- DATN2024/database/migrations/2024_10_31_040622_create_cart_items_table.php
+++ DATN2024/database/migrations/2024_10_31_040622_create_cart_items_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Cart;
+use App\Models\ProductVariant;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(model: Cart::class)->constrained();
+            $table->foreignIdFor(model: ProductVariant::class)->constrained();
+            $table->integer("quantity")->default(1);
+            $table->double("price");
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
