c80d3055e32e7a37cd94e2a0689b5ea7e23d88f5
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 11:30:57 +0700

    Update order_items

--- DATN2024/database/migrations/2024_10_31_034952_create_order_items_table.php
+++ DATN2024/database/migrations/2024_10_31_034952_create_order_items_table.php
@@ -14,19 +14,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('order_items', function (Blueprint $table) {
-            $table->id();
-            $table->foreignIdFor(model: Order::class)->constrained();
-            $table->foreignIdFor(model: ProductVariant::class)->constrained();
-            $table->integer("quantity")->default(0);
-            $table->string("product_name", 255)->nullable();
-            $table->string("product_sku", 255)->nullable();
-            $table->string(column: "product_img_thumbnail")->nullable();
-            $table->double("product_price_regular")->nullable();
-            $table->double("product_price_sale")->nullable();
-            $table->string("variant_size_name", 255)->nullable();
-            $table->string("variant_color_name", 255)->nullable();
-            $table->timestamps();
-            $table->softDeletes();
+            // $table->id();
+            // $table->foreignIdFor(model: Order::class)->constrained();
+            // $table->foreignIdFor(model: ProductVariant::class)->constrained();
+            // $table->integer("quantity")->default(0);
+            // $table->string("product_name", 255)->nullable();
+            // $table->string("product_sku", 255)->nullable();
+            // $table->string(column: "product_img_thumbnail")->nullable();
+            // $table->double("product_price_regular")->nullable();
+            // $table->double("product_price_sale")->nullable();
+            // $table->string("variant_size_name", 255)->nullable();
+            // $table->string("variant_color_name", 255)->nullable();
+            // $table->timestamps();
+            // $table->softDeletes();
         });
     }
 
