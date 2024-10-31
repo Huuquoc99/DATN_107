e3cbb206c6c4b285559a489e1c03840d443c4212
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 15:35:36 +0700

    Show invoice

--- DATN2024/app/Http/Controllers/Admin/InvoiceController.php
+++ DATN2024/app/Http/Controllers/Admin/InvoiceController.php
@@ -8,62 +8,6 @@ use App\Http\Controllers\Controller;
 
 class InvoiceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
-
     public function getInvoices(Request $request)
     {
         $invoices = Order::where('status_payment_id', 5)
@@ -71,5 +15,20 @@ class InvoiceController extends Controller
             ->get(['id as order_id', 'user_name', 'total_price', 'created_at']);
 
         return response()->json($invoices);
+    }
+
+    public function showInvoice($id)
+    {
+        $order = Order::with('orderItems')
+            ->where('id', $id)
+            ->where('status_payment_id', 5)
+            ->where('status_order_id', 5)
+            ->first();
+
+        if (!$order) {
+            return response()->json(['error' => 'Invoice not found or not available'], 404);
+        }
+
+        return response()->json($order);
     }
 }
