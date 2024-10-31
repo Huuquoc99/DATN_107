0405f049fdea3e6b46889f9af4f12bab8169b6e7
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 10:22:24 +0700

    Update migration product variants

--- DATN2024/database/migrations/2024_10_31_032012_create_product_variants_table.php
+++ DATN2024/database/migrations/2024_10_31_032012_create_product_variants_table.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Product;
+use App\Models\ProductColor;
+use App\Models\ProductCapacity;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +16,16 @@ return new class extends Migration
     {
         Schema::create('product_variants', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(model: Product::class)->constrained();
+            $table->foreignIdFor(model: ProductCapacity::class)->constrained();
+            $table->foreignIdFor(model: ProductColor::class)->constrained();
+            $table->unsignedInteger('quantity')->default(0);
+            $table->string('image', 255)->nullable();
+            $table->string("sku", 255)->nullable();
+            $table->double("price")->nullable();
+            $table->tinyInteger("status")->default(1);
             $table->timestamps();
+            $table->unique(['product_id', 'product_capacity_id', 'product_color_id'], 'product_variant_unique');
         });
     }
 
