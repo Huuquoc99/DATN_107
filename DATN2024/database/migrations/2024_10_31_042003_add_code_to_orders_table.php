caecdbf52e8d56c43b73b8258f311a31dca09a58
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 11:21:07 +0700

    Update code

--- DATN2024/database/migrations/2024_10_31_042003_add_code_to_orders_table.php
+++ DATN2024/database/migrations/2024_10_31_042003_add_code_to_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->string('code')->after('user_id')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->dropColumn('code');
         });
     }
 };
