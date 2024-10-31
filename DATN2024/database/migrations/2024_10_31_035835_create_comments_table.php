5b115f95d65f18382c180d0f7b3040433a96a14d
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 11:00:41 +0700

    Update migration comments

--- DATN2024/database/migrations/2024_10_31_035835_create_comments_table.php
+++ DATN2024/database/migrations/2024_10_31_035835_create_comments_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(model: User::class)->constrained();
+            $table->foreignIdFor(model: Product::class)->constrained();
+            $table->text("content");
+            $table->tinyInteger("is_active")->default(1);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
