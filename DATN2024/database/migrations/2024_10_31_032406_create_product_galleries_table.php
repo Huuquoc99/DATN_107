6d29c3e6c7ac470c8ab10bab72db4d3f6dd39daa
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 10:33:18 +0700

    Update migration product galleries

--- DATN2024/database/migrations/2024_10_31_032406_create_product_galleries_table.php
+++ DATN2024/database/migrations/2024_10_31_032406_create_product_galleries_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Product;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +14,10 @@ return new class extends Migration
     {
         Schema::create('product_galleries', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(model: Product::class)->constrained();
+            $table->string('image', 255);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
