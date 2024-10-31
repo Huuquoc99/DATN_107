a28674105d474064908a3dcafd77e908cdf342d7
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 10:49:04 +0700

    Update migration orders

--- DATN2024/database/migrations/2024_10_31_034612_create_orders_table.php
+++ DATN2024/database/migrations/2024_10_31_034612_create_orders_table.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
+use App\Models\StatusOrder;
+use App\Models\PaymentMethod;
+use App\Models\StatusPayment;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +17,24 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(model: User::class)->constrained();
+            $table->string("user_name", 255);
+            $table->string("user_email", 255);
+            $table->string("user_phone", 255);
+            $table->string("user_address", 255);
+            $table->string("user_note", 255)->nullable();
+            $table->tinyInteger("is_ship_user_same_user")->default(1);
+            $table->string("ship_user_name", 255)->nullable();
+            $table->string("ship_user_email", 255)->nullable();
+            $table->string("ship_user_phone", 255)->nullable();
+            $table->string("ship_user_address", 255)->nullable();
+            $table->string("ship_user_note", 255)->nullable();
+            $table->foreignIdFor(model: StatusOrder::class)->constrained();
+            $table->foreignIdFor(model: StatusPayment::class)->constrained();
+            $table->foreignIdFor(model: PaymentMethod::class)->constrained();
+            $table->double("total_price", 10, 2);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
