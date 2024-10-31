49d327cfefbbf166e2b97dd384f4dea4546aac1a
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 16:02:02 +0700

    list status order

--- DATN2024/app/Http/Controllers/Admin/StatusOrderController.php
+++ DATN2024/app/Http/Controllers/Admin/StatusOrderController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\StatusOrder;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class StatusOrderController extends Controller
 {
@@ -12,7 +13,8 @@ class StatusOrderController extends Controller
      */
     public function index()
     {
-        //
+        $listStatusOrder = StatusOrder::get();
+        return response()->json( $listStatusOrder, 201);
     }
 
     /**
