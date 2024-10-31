73f5f4014577cc7e3c9b579c30ac751d3148a48d
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 15:36:41 +0700

    List order

--- DATN2024/app/Http/Controllers/Admin/OrderController.php
+++ DATN2024/app/Http/Controllers/Admin/OrderController.php
@@ -2,10 +2,40 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class OrderController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $orders = Order::with('user', 'statusOrder', 'statusPayment', 'orderItems')->get();
+
+        // Lọc theo trạng thái 
+        if ($request->has('status')) {
+            $orders = $orders->where('status_order_id', $request->input('status'));
+        }
+
+        $result = $orders->map(function ($order) {
+            return [
+                'order_id' => $order->id,
+                'code' => $order->code,
+                'user_name' => $order->user_name,
+                'status_order' => $order->statusOrder->name ?? 'Unknown', 
+                'status_payment' => $order->statusPayment->name ?? 'Unknown', 
+                'total_price' => $order->total_price,
+                'orderItems' => $order->orderItems->map(function ($orderItems) {
+                    return [
+                        'product_name' => $orderItems->product_name,
+                        'product_img_thumbnail' => $orderItems->product_img_thumbnail,
+                        'product_price' => $orderItems->product_price_regular,
+                        'quantity' => $orderItems->quantity,
+                    ];
+                }),
+            ];
+        });
+
+        return response()->json($result);
+    }
 }
