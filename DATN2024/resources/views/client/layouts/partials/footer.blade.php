162ba1d168f5f6b37e294518465c997a4567969c
NguyenQuangTung1001 <tunglm100@gmail.com>
2024-10-31 22:51:17 +0700

    view checkout

--- DATN2024/resources/views/client/layouts/partials/footer.blade.php
+++ DATN2024/resources/views/client/layouts/partials/footer.blade.php
@@ -1,1 +1,107 @@
-footer
+<footer class="footer footer_type_1 dark theme-bg-color ">
+    <div class="footer-top container">
+      <div class="block-newsletter mb-4">
+        <h3 class="block__title text-uppercase text-white fs-30 fw-semi-bold mb-2">Subscribe to our newsletter</h3>
+        <p class="mb-4">Be the first to get the latest news about trends, promotions, and much more!</p>
+
+        <div class="mb-2 pb-1"></div>
+        <form action="https://uomo-html.flexkitux.com/Demo18/index.html" class="block-newsletter__form">
+          <input class="form-control border-radius-8" type="email" name="email" placeholder="Your email address">
+          <button class="btn btn-secondary fw-semi-bold fs-base theme-bg-color-secondary theme-color border-0 border-radius-8" type="submit">JOIN</button>
+        </form>
+      </div>
+
+      <div class="mb-3 pb-2"></div>
+      <ul class="sub-menu__list list-unstyled d-flex gap-4 align-items-center justify-content-center">
+        <li class="sub-menu__item"><a href="terms.html" class="menu-link menu-link_us-s">Returns Policy</a></li>
+        <li class="sub-menu__item"><a href="shop_order_tracking.html" class="menu-link menu-link_us-s">Track Your Order</a></li>
+        <li class="sub-menu__item"><a href="about.html" class="menu-link menu-link_us-s">Shipping & Delivery</a></li>
+      </ul>
+
+      <ul class="social-links list-unstyled d-flex flex-wrap mb-0 align-items-center justify-content-center">
+        <li>
+          <a href="https://www.facebook.com/" class="footer__social-link d-block">
+            <svg class="svg-icon svg-icon_facebook" width="9" height="15" viewBox="0 0 9 15" xmlns="http://www.w3.org/2000/svg"><use href="#icon_facebook"></use></svg>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/" class="footer__social-link d-block">
+            <svg class="svg-icon svg-icon_twitter" width="14" height="13" viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg"><use href="#icon_twitter"></use></svg>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/" class="footer__social-link d-block">
+            <svg class="svg-icon svg-icon_instagram" width="14" height="13" viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg"><use href="#icon_instagram"></use></svg>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/" class="footer__social-link d-block">
+            <svg class="svg-icon svg-icon_youtube" width="16" height="11" viewBox="0 0 16 11" xmlns="http://www.w3.org/2000/svg"><path d="M15.0117 1.8584C14.8477 1.20215 14.3281 0.682617 13.6992 0.518555C12.5234 0.19043 7.875 0.19043 7.875 0.19043C7.875 0.19043 3.19922 0.19043 2.02344 0.518555C1.39453 0.682617 0.875 1.20215 0.710938 1.8584C0.382812 3.00684 0.382812 5.46777 0.382812 5.46777C0.382812 5.46777 0.382812 7.90137 0.710938 9.07715C0.875 9.7334 1.39453 10.2256 2.02344 10.3896C3.19922 10.6904 7.875 10.6904 7.875 10.6904C7.875 10.6904 12.5234 10.6904 13.6992 10.3896C14.3281 10.2256 14.8477 9.7334 15.0117 9.07715C15.3398 7.90137 15.3398 5.46777 15.3398 5.46777C15.3398 5.46777 15.3398 3.00684 15.0117 1.8584ZM6.34375 7.68262V3.25293L10.2266 5.46777L6.34375 7.68262Z"></path></svg>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.pinterest.com/" class="footer__social-link d-block">
+            <svg class="svg-icon svg-icon_pinterest" width="14" height="15" viewBox="0 0 14 15" xmlns="http://www.w3.org/2000/svg"><use href="#icon_pinterest"></use></svg>
+          </a>
+        </li>
+      </ul>
+
+      <div class="mb-3 mb-xl-5 pb-3 pt-1 pb-xl-4"></div>
+    </div>
+
+    <div class="footer-bottom">
+      <div class="d-block d-md-flex align-items-center container">
+        <span class="footer-copyright me-auto">©2023 Uomo</span>
+        <div class="footer-settings d-block d-md-flex align-items-center">
+          <div class="d-flex align-items-center">
+            <label for="footerSettingsLanguage" class="me-2 text-white">Language</label>
+            <select id="footerSettingsLanguage" class="form-select form-select-sm bg-transparent border-0" aria-label="Default select example" name="store-language">
+              <option class="footer-select__option" selected>United Kingdom | English</option>
+              <option class="footer-select__option" value="1">United States | English</option>
+              <option class="footer-select__option" value="2">German</option>
+              <option class="footer-select__option" value="3">French</option>
+              <option class="footer-select__option" value="4">Swedish</option>
+            </select>
+          </div>
+
+          <div class="d-flex align-items-center">
+            <label for="footerSettingsCurrency" class="ms-md-3 me-2 text-white">Currency</label>
+            <select id="footerSettingsCurrency" class="form-select form-select-sm bg-transparent border-0" aria-label="Default select example" name="store-language">
+              <option selected>$ USD</option>
+              <option value="1">£ GBP</option>
+              <option value="2">€ EURO</option>
+            </select>
+          </div>
+        </div><!-- /.footer-settings -->
+      </div><!-- /.d-flex -->
+    </div><!-- /.footer-bottom container -->
+  </footer>
+
+  <!-- Mobile Fixed Footer -->
+  <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
+    <div class="row text-center">
+      <div class="col-4">
+        <a href="https://uomo-html.flexkitux.com/" class="footer-mobile__link d-flex flex-column align-items-center">
+          <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#icon_home" /></svg>
+          <span>Home</span>
+        </a>
+      </div><!-- /.col-3 -->
+
+      <div class="col-4">
+        <a href="https://uomo-html.flexkitux.com/" class="footer-mobile__link d-flex flex-column align-items-center">
+          <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#icon_hanger" /></svg>
+          <span>Shop</span>
+        </a>
+      </div><!-- /.col-3 -->
+
+      <div class="col-4">
+        <a href="https://uomo-html.flexkitux.com/" class="footer-mobile__link d-flex flex-column align-items-center">
+          <div class="position-relative">
+            <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#icon_heart" /></svg>
+            <span class="wishlist-amount d-block position-absolute js-wishlist-count">3</span>
+          </div>
+          <span>Wishlist</span>
+        </a>
+      </div><!-- /.col-3 -->
+    </div><!-- /.row -->
+  </footer><!-- /.footer-mobile container position-fixed d-md-none bottom-0 -->
--- DATN2024/routes/web.php
+++ DATN2024/routes/web.php
@@ -21,6 +21,7 @@ Route::get('home', [App\Http\Controllers\Client\HomeController::class, 'index'])
 Route::get('register', [App\Http\Controllers\Client\HomeController::class, 'register']);
 Route::get('login', [App\Http\Controllers\Client\HomeController::class, 'login'])->name('login');
 Route::get('reset_password', [App\Http\Controllers\Client\HomeController::class, 'resetpassword']);
+Route::get('checkout', [App\Http\Controllers\Client\HomeController::class, 'checkout']);
 
 Route::get('about', [App\Http\Controllers\Client\HomeController::class, 'about']);
 Route::get('contact', [App\Http\Controllers\Client\HomeController::class, 'contact']);
