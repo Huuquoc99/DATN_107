55a208d064006f04d44bf0bf966687722ddc632a
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 10:53:40 +0700

    Update migration order_items

--- DATN2024/database/migrations/2024_10_31_034952_create_order_items_table.php
+++ DATN2024/database/migrations/2024_10_31_034952_create_order_items_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Order;
+use App\Models\ProductVariant;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +15,18 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(model: Order::class)->constrained();
+            $table->foreignIdFor(model: ProductVariant::class)->constrained();
+            $table->integer("quantity")->default(0);
+            $table->string("product_name", 255)->nullable();
+            $table->string("product_sku", 255)->nullable();
+            $table->string(column: "product_img_thumbnail")->nullable();
+            $table->double("product_price_regular")->nullable();
+            $table->double("product_price_sale")->nullable();
+            $table->string("variant_size_name", 255)->nullable();
+            $table->string("variant_color_name", 255)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
