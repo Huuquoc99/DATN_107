02bbefdc885b33170e691168dd4d2076d40edf07
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-31 16:12:00 +0700

    restore trash

--- DATN2024/app/Http/Controllers/Admin/TrashedController.php
+++ DATN2024/app/Http/Controllers/Admin/TrashedController.php
@@ -13,4 +13,28 @@ class TrashedController extends Controller
         $trashed = Product::onlyTrashed()->get();
         return response()->json($trashed);
     }
+
+    public function restore($id)
+    {
+        $product = Product::withTrashed()->findOrFail($id);
+        $product->restore();
+
+        return response()->json(['message' => 'Product restore successful']);
+    }
+
+    // public function forceDelete($id)
+    // {
+    //     $product = Product::withTrashed()->findOrFail($id);
+
+    //     if ($product->img_thumbnail && Storage::disk('public')->exists($product->img_thumbnail)) {
+    //         Storage::disk('public')->delete($product->img_thumbnail);
+    //     }
+
+    //     // Xoá bình luận có liên quan thông qua product_id
+    //     $product->comments()->delete();
+
+    //     $product->forceDelete();
+
+    //     return response()->json(['message' => 'Permanently delete product successfully']);
+    // }
 }
