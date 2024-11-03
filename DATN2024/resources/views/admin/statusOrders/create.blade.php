88339504986bfa5925712843699d4bc52d9450cb
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-11-03 13:41:46 +0700

    Update status orders

--- DATN2024/resources/views/admin/statusOrders/create.blade.php
+++ DATN2024/resources/views/admin/statusOrders/create.blade.php
@@ -2,19 +2,19 @@
 @extends('admin.layouts.master')
 
 @section('title')
-Status order create
+Status order 
 @endsection
 
 @section('content')
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0">Status order create</h4>
+                <h4 class="mb-sm-0">Status order </h4>
 
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">Status order</a></li>
-                        <li class="breadcrumb-item active">Status order create</li>
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Table </a></li>
+                        <li class="breadcrumb-item active">  Create</li>
                     </ol>
                 </div>
 
@@ -52,7 +52,7 @@ Status order create
                                         <div class="mt-4">
                                             @php
                                                 $is = [
-                                                    'is_active' => ['name' => 'Kích hoạt', 'color' => 'primary'],
+                                                    'is_active' => ['name' => 'Active ', 'color' => 'primary'],
                                                 ];
                                             @endphp
                                             @foreach($is as $key => $value)
