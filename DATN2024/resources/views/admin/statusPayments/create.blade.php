55713de8791782ebf04170c86e067b7f82355f59
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-11-17 16:53:57 +0700

    Update status order

--- DATN2024/resources/views/admin/statusPayments/create.blade.php
+++ DATN2024/resources/views/admin/statusPayments/create.blade.php
@@ -36,18 +36,22 @@ Status Payment
                                 <div class="col-md-6">
                                     <div>
                                         <label for="name" class="form-label">Name</label>
-                                        <input type="text" class="form-control" name="name" id="name">
-                                        @error("name") 
-                                            <div class="alert alert-danger alert-dismissible fade show mt-4" style="height: 45px;" role="alert">
-                                                <p class="text-danger">{{ $message }}</p>
-                                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                            </div>
+                                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name">
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
                                         @enderror
                                     </div>
                                     <div class="row">
                                         <div class="mt-3">
                                             <label for="display_order" class="form-label">Display order</label>
-                                            <input type="number" class="form-control" name="display_order" id="display_order">
+                                            <input type="number" class="form-control @error('display_order') is-invalid @enderror" name="display_order" id="display_order">
+                                            @error('display_order')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
                                         <div class="mt-4">
                                             @php
@@ -77,17 +81,21 @@ Status Payment
                                     
                                     <div class="mt-3">
                                         <label for="code" class="form-label">Code</label>
-                                        <input type="text" class="form-control" name="code" id="code">
-                                        @error("code") 
-                                            <div class="alert alert-danger alert-dismissible fade show mt-4" style="height: 45px;" role="alert">
-                                                <p class="text-danger">{{ $message }}</p>
-                                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                            </div>
+                                        <input type="text" class="form-control @error('code') is-invalid @enderror" name="code" id="code">
+                                        @error('code')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
                                         @enderror
                                     </div>
                                     <div class="mt-3">
                                         <label for="description" class="form-label">Description</label>
-                                        <textarea class="form-control" name="description" id="description" rows="2"></textarea>
+                                        <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="2"></textarea>
+                                        @error('description')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                 </div>
                             </div>
--- DATN2024/resources/views/admin/statusPayments/index.blade.php
+++ DATN2024/resources/views/admin/statusPayments/index.blade.php
@@ -63,13 +63,28 @@
                                         <td>{{ $item->code }}</td>
                                         <td>
                                             <a href="{{ route('admin.statusPayments.edit', $item) }}">
-                                                {{ $item->name }}
+                                                {{ \Illuminate\Support\Str::limit($item->name, 15, '...') }}
                                             </a>
                                         </td>
                                         <td>{{ $item->display_order }}</td>
                                         <td>{!! $item->is_active ? '<span class="badge bg-primary">Active</span>' : '<span class="badge bg-danger">No active</span>' !!}</td>
-                                        <td>{{ $item->created_at }}</td>
-                                        <td>{{ $item->updated_at }}</td>
+                                        <td>
+                                            @if($item->created_at)
+                                                <span id="invoice-date">{{ $item->created_at->format('d M, Y') }}</span>
+                                                <small class="text-muted" id="invoice-time">{{ $item->created_at->format('h:iA') }}</small>
+                                            @else
+                                                <span class="text-muted">N/A</span>
+                                            @endif
+                                        </td>
+                                        
+                                        <td>
+                                            @if($item->updated_at)
+                                                <span id="invoice-date">{{ $item->updated_at->format('d M, Y') }}</span>
+                                                <small class="text-muted" id="invoice-time">{{ $item->updated_at->format('h:iA') }}</small>
+                                            @else
+                                                <span class="text-muted">N/A</span>
+                                            @endif
+                                        </td>
                                         <td>
                                             <div class="d-flex gap-2 justify-content-center">
                                                 {{-- <a href="{{ route('admin.statusPayments.show', $item) }}" class="btn btn-info btn-sm">Show 
