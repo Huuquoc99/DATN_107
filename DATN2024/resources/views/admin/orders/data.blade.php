0892154136e92abb1922f2666a0ae141105b9927
kk <>
2024-11-20 10:40:23 +0700

    view data

---
+++ DATN2024/resources/views/admin/orders/data.blade.php
@@ -0,0 +1,19 @@
+@if(!empty($orders))
+    <div class="table-responsive table-card mb-1 text-center">
+        @if (session("success"))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session("success")}}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+        
+    </div>
+@else
+    <div class="noresult">
+        <div class="text-center">
+            <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop" colors="primary:#405189,secondary:#0ab39c" style="width:75px;height:75px"></lord-icon>
+            <h5 class="mt-2">Sorry! No Result Found</h5>
+            <p class="text-muted">We've searched more than 150+ Orders We did not find any orders for you search.</p>
+        </div>
+    </div>
+@endif
