757857bff0739237fd46b3015b706e38d9bf0534
NguyenQuangTung1001 <tunglm100@gmail.com>
2024-11-26 14:27:15 +0700

    Update order model

--- DATN2024/app/Models/Order.php
+++ DATN2024/app/Models/Order.php
@@ -58,4 +58,9 @@ class Order extends Model
     {
         return $this->belongsTo(PaymentMethod::class);
     }
+
+    public function voucher()
+    {
+        return $this->belongsTo(Voucher::class);
+    }
 }
