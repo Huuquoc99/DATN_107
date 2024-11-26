f74943196da3c7fded97c55fe1a0e309d3a8e173
NguyenQuangTung1001 <tunglm100@gmail.com>
2024-11-26 14:30:57 +0700

    Voucher controller

--- DATN2024/app/Http/Controllers/Client/VoucherController.php
+++ DATN2024/app/Http/Controllers/Client/VoucherController.php
@@ -14,6 +14,22 @@ class VoucherController extends Controller
         $catalogues = Catalogue::where('is_active', 1)->get();
         $vouchers = Voucher::query()->active()->whereColumn('used_quantity', '<', 'quantity')->get();
         // dd($vouchers);
-        return view('client.vouchers', compact('catalogues','vouchers'));
+        return view('client.vouchers', compact('catalogues', 'vouchers'));
+    }
+
+    public function applyVoucher(ApplyVoucherRequest $request)
+    {
+        // clear session voucher
+        session()->forget('voucher');
+        $voucher = Voucher::query()->active()->where('code', $request->code)->first();
+        if (!$voucher) {
+            return response()->json(['message' => 'Mã giảm giá không tồn tại'], 404);
+        }
+
+        if ($voucher->used_quantity >= $voucher->quantity) {
+            return response()->json(['message' => 'Mã giảm giá đã hết'], 404);
+        }
+        session()->put('voucher', $voucher->code);
+        return response()->json($voucher);
     }
 }
