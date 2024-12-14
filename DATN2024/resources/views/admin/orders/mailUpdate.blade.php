15a64967b81babf17857c37fca3a1a80473a7888
kk <>
2024-12-14 17:41:44 +0700

    Fix conflict mailCancle, mailUpdate

--- DATN2024/resources/views/admin/orders/mailUpdate.blade.php
+++ DATN2024/resources/views/admin/orders/mailUpdate.blade.php
@@ -75,7 +75,6 @@
 
 <body>
 
-<<<<<<< HEAD
 <div class="container">
     <div class="header">
         <h2 style="padding-left: 20px">Thông báo cập nhật trạng thái đơn hàng</h2>
@@ -110,51 +109,6 @@
         </p>
 
         <p>Cảm ơn bạn rất nhiều vì đã sử dụng dịch vụ của chúng tôi!</p>
-=======
-    <div class="container">
-        <div class="header">
-            <h2 style="padding-left: 20px">Thông báo cập nhật trạng thái đơn hàng</h2>
-        </div>
-
-        <div class="content">
-            <p>
-                Xin chào
-                <strong>
-                    {{ $order->user ? $order->user->name : $order->ship_user_name }}
-                </strong>,
-            </p>
-            <p>
-                Chúng tôi muốn thông báo với bạn rằng đơn hàng có mã
-                <strong>{{ $order->code }}</strong>
-                đã được quản trị viên cập nhật thành trạng thái:
-                <strong>{{ $order->statusOrder->name }}</strong>
-            </p>
-
-            <div class="order-details">
-                <p><strong>Chi tiết đơn hàng:</strong></p>
-                <p>Mã đơn hàng: <strong>{{ $order->code }}</strong></p>
-                <p>Trạng thái: <strong>{{ $order->statusOrder->name }}</strong></p>
-            </div>
-
-            <p>
-                @if ($order->user)
-                    Nếu bạn có bất kỳ câu hỏi nào hoặc cần hỗ trợ thêm, vui lòng liên hệ với chúng tôi qua email hoặc số
-                    điện thoại bên dưới.
-                @else
-                    Vì bạn đã đặt đơn hàng này với tư cách là khách, nếu bạn có bất kỳ câu hỏi nào hoặc cần hỗ trợ, vui
-                    lòng cung cấp mã đơn hàng của bạn khi liên hệ với chúng tôi.
-                @endif
-            </p>
-
-            <p>Cảm ơn bạn rất nhiều vì đã sử dụng dịch vụ của chúng tôi!</p>
-        </div>
-
-        <div class="footer">
-            <p><a href="mailto:techstore@gmail.com">Email hỗ trợ</a> | <a href="tel:0987654321">Liên hệ qua điện
-                    thoại</a></p>
-            <p>Trân trọng, TechStore</p>
-        </div>
->>>>>>> hoa04
     </div>
 
 </body>
